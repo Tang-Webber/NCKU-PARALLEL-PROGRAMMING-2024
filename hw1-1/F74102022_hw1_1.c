82547d630800a8fcfea14b86751ac75d530a3b4a
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-09-28 23:11:04 +0800

    hw1_1 draft

---
+++ hw1-1/F74102022_hw1_1.c
@@ -0,0 +1,65 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool f(unsigned int*, unsigned int*, int, int);
+bool f(unsigned int *tests, unsigned int* t, int n, int m){
+    unsigned int set = 0;
+    for(int i = 0; i < m; i++){
+        if ((t & (1u << i)) != 0) 
+            set = set | tests[i];
+    }
+    if(set == (unsigned int)pow(2,n))
+        return true;
+    else
+        return false;
+}
+
+int main( int argc, char *argv[])
+{
+    int n, m, myid, numprocs;
+    int i = 0;
+    int count = 0;
+    int sum;
+    unsigned int ltime;
+    unsigned int tests[32] = {0};
+    double startwtime = 0.0, endwtime;
+    int  namelen;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+    //MPI_Get_processor_name(processor_name,&namelen);
+    if (myid == 0) {
+        //startwtime = MPI_Wtime(); 
+        scanf("%d %d", &n, &m);
+        ltime = (unsigned int)pow(2, n);
+        for (int j = 0; j < m; j++) {
+            int num;
+            scanf("%d", &num);
+            tests[j] = 0;
+            for (int k = 0; k < num; k++) {
+                int part;
+                scanf("%d", &part);
+                tests[j] |= (1 << (part - 1));
+            }
+            MPI_Bcast(&tests[j], 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+        }
+    }
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    for (i = myid + 1; i <= n; i += numprocs){
+        if(f(tests, i, n, m) == true) count++;
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (myid == 0){
+        MPI_Reduce(&count, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+        printf("%d", sum);  
+    } 
+    MPI_Finalize();
+
+    return 0;
+}
