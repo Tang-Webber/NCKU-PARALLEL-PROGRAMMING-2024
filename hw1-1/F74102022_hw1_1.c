43f8cc3270b7926d3d978af1322d42d79b1568c4
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-09-30 00:04:37 +0800

    0929-test3

--- hw1-1/F74102022_hw1_1.c
+++ hw1-1/F74102022_hw1_1.c
@@ -2,25 +2,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-/*
-bool f(unsigned int*, unsigned int, unsigned int, int);
-bool f(unsigned int* tests, unsigned int t, unsigned int pow2n, int m){
-    unsigned int set = 0;
-    for(int i = 0; i < m; i++){
-        if ((t & (1u << i)) != 0) 
-            set = set | tests[i];
-    }
-printf("set = %u, %u, %u\n", set, pow2n, t);
-    if(set == pow2n)
-        return true;
-    else
-        return false;
-}
-*/
+
 int main( int argc, char *argv[])
 {
     int n, m, myid, numprocs, num, part, cost;
     int count = 0;
+    //int* counts = NULL;
     int sum;
     unsigned int pow2n = 1;             //pow2n : process represent in binary
     unsigned int pow2m = 1;             //pow2m : num of premutations
@@ -38,6 +25,7 @@ int main( int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     //MPI_Get_processor_name(processor_name,&namelen);
     if (myid == 0) {
+        //counts = (int*)malloc(numprocs * sizeof(int));
         //startwtime = MPI_Wtime(); 
         scanf("%s", input);
         FILE *input_file = fopen(input, "r");
@@ -56,19 +44,15 @@ printf("open%s!\n", input);
             pow2m *= 2;
         }
         for (int j = 0; j < m; j++) {
-printf("input: %d\n", j);
             fscanf(input_file, "%d %d", &num, &cost);
             tests[j] = 0;
             for (int k = 0; k < num; k++) {
                 fscanf(input_file, "%d", &part);
                 tests[j] |= (1 << (part - 1));
             }
-printf("test[%d] = %u\n", j, tests[j]);
-            //MPI_Bcast(&tests[j], 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-printf("complete %d\n", j);
         }
+        fclose(input_file);
     }
-printf("id:%d", myid);
     MPI_Datatype testarr;
     MPI_Type_contiguous(32, MPI_UNSIGNED, &testarr);
     MPI_Type_commit(&testarr);
@@ -79,7 +63,6 @@ printf("id:%d", myid);
     MPI_Bcast(&pow2m, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD); 
 printf("ID : ---%d--- ,total:%d, Paremeter: %d %d %u %u\n", myid, numprocs, n,m,pow2n,pow2m);   
-    
     for (unsigned int i = myid; i < pow2m; i += numprocs){
         t = i;
         set = 0;
@@ -94,18 +77,21 @@ printf("set=%u, ans=%u \n", set, pow2n);
         //if(i <= pow2m && f(tests, i, pow2n, m)){count++};
     }
 printf("test2 , id : %d\n", myid);   
+    MPI_Barrier(MPI_COMM_WORLD); 
     MPI_Barrier(MPI_COMM_WORLD);  
-printf("test3\n, sum:%d", sum);   
-    MPI_Reduce(&count, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);   
-printf("test4\n, sum:%d", sum);    
+printf("---------B---------\n");   
+    //MPI_Gather(&count, 1, MPI_INT, counts, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&count, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);      
     if (myid == 0){
+        /*
         strcpy(output, input);
         char *dot = strrchr(output, '.');
         if (dot != NULL) {
             *dot = '\0';
         }
         strcat(output, ".out");
-printf("out:%s\n", output);
+        */
+        output = "/home/F74102022/hw1-1/output/data/out.txt";
         FILE *output_file = fopen(output, "w");
 printf("%d\n", sum);
         fprintf(output_file, "%d\n", sum); 
