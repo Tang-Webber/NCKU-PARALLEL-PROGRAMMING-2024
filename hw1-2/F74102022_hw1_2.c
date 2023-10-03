b84d6acf88f9950d4e0873b9eb8818eabf3252f1
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-10-03 20:20:56 +0800

    1003-2 hw1-2 test7

--- hw1-2/F74102022_hw1_2.c
+++ hw1-2/F74102022_hw1_2.c
@@ -191,13 +191,15 @@ int main( int argc, char *argv[])
         for(int i = 0;i < u; i++){
             printf("%d ", final_up[i].id);
         }
-        printf("\nup ||||||| down\n");
-        if(final_up[u].id == final_down[d].id){
-            printf("%d ", final_down[d--].id);
-        }          
+        //if(final_up[u].id == final_down[d].id){
+        //    printf("%d ", final_down[d--].id);
+        //}          
         for(int i = d;i > 0; i--){
             printf("%d ", final_down[i].id);
-        }        
+        } 
+
+printf("up:%d, down: %d\n %d %d %d %d %d ", u, d, n, numprocs, left_count, left_up, left_down );     
+
         //Free memory
         for (int i = 0; i < numprocs + rest; i++) {
             free(gathered_up[i]);
