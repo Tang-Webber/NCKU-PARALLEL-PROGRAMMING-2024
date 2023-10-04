33b45d1bfdeb935f6f058df7ac6b74f5146353da
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-10-04 20:38:55 +0800

    Half conbination to ID=1 test7

--- hw1-2/F74102022_hw1_2.c
+++ hw1-2/F74102022_hw1_2.c
@@ -140,10 +140,10 @@ int main( int argc, char *argv[])
         }
     }
 
-    MPI_Bcast(ups, numprocs + rest, MPI_INT, 0, MPI_COMM_WORLD);
+    //MPI_Bcast(ups, numprocs + rest, MPI_INT, 0, MPI_COMM_WORLD);
 printf("ID:%d ups get !\n", myid);
     //MPI_Bcast(downs, numprocs + rest, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(final_up, n, PointType, 0, MPI_COMM_WORLD);
+    //MPI_Bcast(final_up, n, PointType, 0, MPI_COMM_WORLD);
 printf("ID:%d final_up get!\n", myid);
     //MPI_Bcast(final_down, n, PointType, 0, MPI_COMM_WORLD);    
     //MPI_Bcast(gathered_up, numprocs * (base_count + rest) , PointType, 0, MPI_COMM_WORLD);
