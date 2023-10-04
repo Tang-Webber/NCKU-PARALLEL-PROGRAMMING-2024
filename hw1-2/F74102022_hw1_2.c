16ff6308c5e683e40abf4b8067db425b19763fac
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-10-04 20:33:02 +0800

    Half conbination to ID=1 test6

--- hw1-2/F74102022_hw1_2.c
+++ hw1-2/F74102022_hw1_2.c
@@ -141,11 +141,15 @@ int main( int argc, char *argv[])
     }
 
     MPI_Bcast(ups, numprocs + rest, MPI_INT, 0, MPI_COMM_WORLD);
+printf("ID:%d ups get !\n", myid);
     //MPI_Bcast(downs, numprocs + rest, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(final_up, n, PointType, 0, MPI_COMM_WORLD);
+printf("ID:%d final_up get!\n", myid);
+    //MPI_Bcast(final_down, n, PointType, 0, MPI_COMM_WORLD);    
     MPI_Bcast(gathered_up, numprocs * (base_count + rest) , PointType, 0, MPI_COMM_WORLD);
+printf("ID:%d gathered_up get !\n", myid);    
     //MPI_Bcast(gathered_down, numprocs * (base_count + rest) , PointType, 0, MPI_COMM_WORLD);
-    //MPI_Bcast(final_up, n, PointType, 0, MPI_COMM_WORLD);
-    //MPI_Bcast(final_down, n, PointType, 0, MPI_COMM_WORLD);
+
 
     //Combine small convex hulls
     if (myid == 0){
