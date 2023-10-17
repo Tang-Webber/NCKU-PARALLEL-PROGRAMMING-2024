148ead2817e612d05e2c2f33dddb6eb468ef4875
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-10-18 00:20:46 +0800

    non parallel test 2

--- hw2-2/F74102022_hw2_2.c
+++ hw2-2/F74102022_hw2_2.c
@@ -75,7 +75,7 @@ printf("\n");
 printf("choose dist[%d]=%d", min[0], min[1]);                
                 selected[min[0]] = true;
                 for(int j = 0; j < n; j++){
-                    if(!selected[j] && dist[j] < dist[min[0]] + Adj[min[0]][j]){
+                    if(!selected[j] && dist[j] > dist[min[0]] + Adj[min[0]][j]){
                         dist[min[0]] = Adj[min[0]][j];
                     }
                 }
