2b5134d92c36659df8c8002e19728e106ac7c3a4
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-10-17 23:58:42 +0800

    non parallel test 1

--- hw2-2/F74102022_hw2_2.c
+++ hw2-2/F74102022_hw2_2.c
@@ -5,13 +5,17 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-short Adj[50000][50000];
+short Adj[50000][50000];            //adjacency matrix         
 
 int main( int argc, char *argv[]){
     int n, myid, numprocs;
     char input[50];
-    
-    short **A;
+    int size;
+    int choose[50000] = {0};  
+    bool selected[50000];
+    int dist[50000];   
+    int min[2];         //(index, dist)
+
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
@@ -25,10 +29,16 @@ int main( int argc, char *argv[]){
             return 1;
         }
         fscanf(input_file, "%d", &n);
-        A = (short**)malloc(n  * sizeof(short*));
-        for (int i = 0; i < n; i++) {
-            A[i] = (short*)malloc(n  * sizeof(short*));
-        }    
+  
+        for(int i=0; i<n;i++){
+            for(int j=0;j<n;j++){
+                Adj[i][j] = 1000;
+            }
+            choose[i] = 1000;
+            selected[i] = false;
+        }
+        choose[0] = 0;
+
         int x, y;
         short temp;
         while (!feof(input_file)) {
@@ -37,15 +47,47 @@ int main( int argc, char *argv[]){
         }
         fclose(input_file);
     }
-    if(myid != 0){
-        A = (short**)malloc(n  * sizeof(short*));
-        for (int i = 0; i < n; i++) {
-            A[i] = (short*)malloc(n  * sizeof(short*));
-        }  
-    }
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(Adj, 25000000, MPI_INT, 0, MPI_COMM_WORLD);
-printf("%hd\n", Adj[0][2]);
+    size = n / numprocs;
+
+    if(size == 0){                      //6
+        if(myid == 0){
+            for(int i=0;i<n;i++){           //initialize
+                if(Adj[0][i] != 1000){
+                    dist[i] = Adj[0][i];
+                }
+            }
+            for(int i = 1; i < n; i++){
+                min[1] = 1000;
+                for(int j = 0; j < n; j++){
+                    if(!selected[j] && dist[j] < min[1]){
+                        min[0] = j;
+                        min[1] = dist[j];
+                    }
+                }
+                selected[min[0]] = true;
+                for(int j = 0; j < n; j++){
+                    if(!selected[j] && dist[j] < dist[min[0]] + Adj[min[0]][j]){
+                        dist[min[0]] = Adj[min[0]][j];
+                    }
+                }
+            }
+        }
+    }
+    else{                       //1000 50000
+
+    }
+
+//printf("%hd\n", Adj[0][2]);
+    if(myid == 0){
+        for(int i=0;i<n;i++){
+            printf("%d ", dist[i]);
+        }
+    }
+
+
+
     MPI_Finalize();
     return 0;
 }
