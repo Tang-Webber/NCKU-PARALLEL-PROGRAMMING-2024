78185a0e30c5c6f58d8ed75e36c34854d3162fe2
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-10-18 22:27:15 +0800

    hw2-2 mpi test 24

--- hw2-2/F74102022_hw2_2.c
+++ hw2-2/F74102022_hw2_2.c
@@ -54,7 +54,7 @@ int main( int argc, char *argv[]){
         fclose(input_file);
     }
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(Adj, n * n, MPI_SHORT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(Adj, 2500000000, MPI_SHORT, 0, MPI_COMM_WORLD);
     
     for(int i=0; i<n;i++){
         dist[i] = 100000;
