6ecf71b7fe5d26423cc867769759226b9325802d
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-10-18 23:55:57 +0800

    hw2-2 mpi test 54

--- hw2-2/F74102022_hw2_2.c
+++ hw2-2/F74102022_hw2_2.c
@@ -53,6 +53,10 @@ int main( int argc, char *argv[]){
         fclose(input_file);
     }
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    for(int i=0;i<n;i++){
+        MPI_Bcast(Adj[i], n , MPI_SHORT, 0, MPI_COMM_WORLD);
+    }
+    //MPI_Bcast(Adj, 2500000000 , MPI_SHORT, 0, MPI_COMM_WORLD);
     
     for(int i=0; i<n;i++){
         dist[i] = 100000;
@@ -89,18 +93,17 @@ int main( int argc, char *argv[]){
     MPI_Op custom_op;
     MPI_Op_create((MPI_User_function *)custom_min, 1, &custom_op);
     int global_min[2];
-printf("test1\n");
+
     if(size > 70){                         //1000 50000
         //each process calculate n / numprocs , loop start from myid * size      
         selected[0] = true;
         dist[0] = 0;
-        MPI_Bcast(Adj[0], n , MPI_SHORT, 0, MPI_COMM_WORLD);
         for(int i=0;i<n;i++){           //initialize
             if(Adj[0][i] != -1){
                 dist[i] = Adj[0][i];
             }
         }
-printf("test2\n");
+
         for(int i = 1; i < n; i++){
             min[1] = global_min[1] = 100000;
             for(int j = 0; j < size; j++){
@@ -109,10 +112,10 @@ printf("test2\n");
                     min[1] = dist[myid * size + j];
                 }
             }        
-printf("test 3-%d\n", i);
+
             MPI_Allreduce(min, global_min, 2, MPI_INT, custom_op, MPI_COMM_WORLD);
             selected[global_min[0]] = true;
-            //MPI_Bcast(Adj[global_min[0]], n , MPI_SHORT, 0, MPI_COMM_WORLD);
+
             for(int j = 0; j < size; j++){
                 if(!selected[myid * size + j] && Adj[global_min[0]][myid * size + j] != -1 && dist[myid * size + j] > dist[global_min[0]] + Adj[global_min[0]][myid * size + j]){
                     dist[myid * size + j] = dist[global_min[0]] + Adj[global_min[0]][myid * size + j];
@@ -136,7 +139,7 @@ printf("test 3-%d\n", i);
         }
     }
 
-    //MPI_Op_free(&custom_op);
+    MPI_Op_free(&custom_op);
     MPI_Finalize();
     return 0;
 }
