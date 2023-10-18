04601a1a24589528cbc0b169266b63eb0a1be506
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-10-18 22:23:51 +0800

    hw2-2 mpi test 21

--- hw2-2/F74102022_hw2_2.c
+++ hw2-2/F74102022_hw2_2.c
@@ -54,9 +54,9 @@ int main( int argc, char *argv[]){
         fclose(input_file);
     }
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    //for(int i=0;i<n;i++){
-    //    MPI_Bcast(Adj[i], n * sizeof(short), MPI_BYTE, 0, MPI_COMM_WORLD);
-    //}
+    for(int i=0;i<n;i++){
+        MPI_Bcast(&Adj[i][0], n * sizeof(short), MPI_BYTE, 0, MPI_COMM_WORLD);
+    }
     
 
     for(int i=0; i<n;i++){
@@ -66,7 +66,7 @@ int main( int argc, char *argv[]){
 
 
     size = n / numprocs;
-    if(size >= 0){                      //6
+    if(size == 0){                      //6
         if(myid == 0){
             selected[0] = true;
             dist[0] = 0;
@@ -97,7 +97,7 @@ int main( int argc, char *argv[]){
     int global_min[2];
 
     //MPI_Reduce(min, &result, 2, MPI_INT, custom_op, 0, MPI_COMM_WORLD);
-    if(size < 0){                         //1000 50000
+    if(size > 0){                         //1000 50000
         //each process calculate n / numprocs , loop start from myid * size      
         selected[0] = true;
         dist[0] = 0;
