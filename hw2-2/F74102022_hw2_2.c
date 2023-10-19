886abc92d080b8a8e8f7ebb0bdc8f77ecf17957c
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-10-19 08:46:22 +0800

    hw2-2 mpi test 71

--- hw2-2/F74102022_hw2_2.c
+++ hw2-2/F74102022_hw2_2.c
@@ -97,8 +97,8 @@ int main( int argc, char *argv[]){
         int end = start + size; 
         selected[0] = true;
         dist[0] = 0;
-        //MPI_Bcast(Adj[0], n , MPI_SHORT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&Adj[0][myid * size], size , MPI_SHORT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(Adj[0], n , MPI_SHORT, 0, MPI_COMM_WORLD);
+        //MPI_Bcast(&Adj[0][myid * size], size , MPI_SHORT, 0, MPI_COMM_WORLD);
         for(int j = start; j < end; j++){           //initialize
             if(Adj[0][j] != -1){
                 dist[j] = Adj[0][j];
@@ -117,7 +117,7 @@ int main( int argc, char *argv[]){
             MPI_Allreduce(min, global_min, 2, MPI_INT, custom_op, MPI_COMM_WORLD);
             selected[global_min[0]] = true;
             //MPI_Bcast(Adj[global_min[0]], n , MPI_SHORT, 0, MPI_COMM_WORLD);
-            MPI_Bcast(&Adj[global_min[0]][myid * size], size , MPI_SHORT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(&Adj[global_min[0]][start], size , MPI_SHORT, 0, MPI_COMM_WORLD);
             for(int j = start; j < end; j++){
                 if(!selected[j] && Adj[global_min[0]][j] != -1 && dist[j] > global_min[1] + Adj[global_min[0]][j]){
                     dist[j] = global_min[1] + Adj[global_min[0]][j];
