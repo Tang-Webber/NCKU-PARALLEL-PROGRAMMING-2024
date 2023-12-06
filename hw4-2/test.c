055da3d287ba10d33b88dee08c2f0c960831b820
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-12-06 22:04:07 +0800

    hw4-2 test 6

--- hw4-2/test.c
+++ hw4-2/test.c
@@ -26,18 +26,19 @@ int main(int argc, char *argv[]){
         dp[i][i] = 0;
     }
 
-    for (int L = 2; L < n; L++) {
-        for (int i = 1; i < n - L + 1; i++) {
-            int j = i + L - 1;
-
+    for (int len = 2; len < n; len++) {
+        for (int i = 1; i < n - len + 1; i++) {
+            int j = i + len - 1;
             dp[i][j] = 99999;
-            for (int k = i; k < j; k++){
-                if((dp[i][k] + dp[k + 1][j] + neural[i - 1] * neural[k] * neural[j]) < dp[i][j]){
-                    dp[i][j]  = dp[i][k] + dp[k + 1][j] + neural[i - 1] * neural[k] * neural[j];
+            for (int k = i; k < j; k++) {
+                int cost = dp[i][k] + dp[k + 1][j] + neural[i - 1] * neural[k] * neural[j];
+                if (cost < dp[i][j]) {
+                    dp[i][j] = cost;
                 }
             }
         }
     }
+
 for(int i=0;i<n;i++){
     printf("------------------\n");
     for(int j = 0; j<n;j++){
