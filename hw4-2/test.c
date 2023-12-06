d1b96c143aeb70386737bde9c1f9f7012532c8e5
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-12-06 21:47:05 +0800

    hw4-2 test 4

--- hw4-2/test.c
+++ hw4-2/test.c
@@ -38,6 +38,13 @@ int main(int argc, char *argv[]){
             }
         }
     }
+for(int i=0;i<n;i++){
+    printf("------------------\n");
+    for(int j = 0; j<n;j++){
+        printf("%d ", dp[i][j]);
+    }
+    printf("\n------------------\n");
+}
 
     //output
     printf("%d", dp[1][n - 1]);
