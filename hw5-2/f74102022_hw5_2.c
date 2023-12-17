f6f69ca6e82cbe1acde9bf67b16a35e60a6ceda4
Tang-Webber <f74102022@gs.ncku.edu.tw>
2023-12-18 00:54:45 +0800

    hw5-2 test 10

--- hw5-2/f74102022_hw5_2.c
+++ hw5-2/f74102022_hw5_2.c
@@ -129,8 +129,9 @@ int main( int argc, char *argv[])
         //#pragma omp critical
         min(&final, &sum);
     }
-#pragma omp parallel{
+#pragma omp parallel
+    {
     printf("%.4f", final);
-}
+    }
     return 0;
 }
